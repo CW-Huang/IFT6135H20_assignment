bf180dcefc12d809fd94fc4db4aacc357fc4abce
Jonathan Cornford <jonathan.cornford@gmail.com>
2020-02-15 18:59:00 -0500

    remove matplotlib import from solution.py

--- assignment2/solution.py
+++ assignment2/solution.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math, copy, time
 from torch.autograd import Variable
-import matplotlib.pyplot as plt
 
 # NOTE ==============================================
 #
