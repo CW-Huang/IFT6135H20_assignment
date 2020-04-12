c24d5829c300c28c6f793b45aa3c42e199cbb061
Sam <samuel.lavoie.m@gmail.com>
2020-04-12 14:21:53 -0400

    q2 solution

--- assignment3/q2_solution.py
+++ assignment3/q2_solution.py
@@ -24,7 +24,7 @@ def lp_reg(x, y, critic):
     pass
 
 
-def vf_wasserstein_distance(p, q, critic):
+def vf_wasserstein_distance(x, y, critic):
     """
     COMPLETE ME. DONT MODIFY THE PARAMETERS OF THE FUNCTION. Otherwise, tests might fail.
 
@@ -46,13 +46,11 @@ def vf_squared_hellinger(x, y, critic):
     Complete me. DONT MODIFY THE PARAMETERS OF THE FUNCTION. Otherwise, tests might fail.
 
     *** The notation used for the parameters follow the one from Nowazin et al: https://arxiv.org/pdf/1606.00709.pdf
-    In other word, x are samples from the distribution P and y are samples from the distribution Q. The critic is the
-    equivalent of T in the paper. This is important to consider, because we make the assuption that your implementation
-    follows this notation when testing your function. ***
+    In other word, x are samples from the distribution P and y are samples from the distribution Q. Please note that the Critic is unbounded. ***
 
     :param p: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution p.
     :param q: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution q.
-    :param critic: (Module) - torch module used to compute the Peason Chi square.
+    :param critic: (Module) - torch module used to compute the Quarred Hellinger.
     :return: (FloatTensor) - shape: (1,) - Estimate of the Squared Hellinger
     """
     pass
