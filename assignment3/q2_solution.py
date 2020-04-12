ee2dd9aa1aa1fb2632f93ba87fe1555d9b6afb26
Sam <samuel.lavoie.m@gmail.com>
2020-04-12 14:29:37 -0400

    q2 solution

--- assignment3/q2_solution.py
+++ assignment3/q2_solution.py
@@ -16,8 +16,8 @@ def lp_reg(x, y, critic):
     equivalent of f in the paper. Also consider that the norm used is the L2 norm. This is important to consider,
     because we make the assumption that your implementation follows this notation when testing your function. ***
 
-    :param x: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution P.
-    :param y: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution Q.
+    :param x: (FloatTensor) - shape: (batchsize x featuresize) - Samples from a distribution P.
+    :param y: (FloatTensor) - shape: (batchsize x featuresize) - Samples from a distribution Q.
     :param critic: (Module) - torch module that you want to regularize.
     :return: (FloatTensor) - shape: (1,) - Lipschitz penalty
     """
@@ -33,8 +33,8 @@ def vf_wasserstein_distance(x, y, critic):
     equivalent of f in the paper. This is important to consider, because we make the assuption that your implementation
     follows this notation when testing your function. ***
 
-    :param p: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution p.
-    :param q: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution q.
+    :param p: (FloatTensor) - shape: (batchsize x featuresize) - Samples from a distribution p.
+    :param q: (FloatTensor) - shape: (batchsize x featuresize) - Samples from a distribution q.
     :param critic: (Module) - torch module used to compute the Wasserstein distance
     :return: (FloatTensor) - shape: (1,) - Estimate of the Wasserstein distance
     """
@@ -48,8 +48,8 @@ def vf_squared_hellinger(x, y, critic):
     *** The notation used for the parameters follow the one from Nowazin et al: https://arxiv.org/pdf/1606.00709.pdf
     In other word, x are samples from the distribution P and y are samples from the distribution Q. Please note that the Critic is unbounded. ***
 
-    :param p: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution p.
-    :param q: (FloatTensor) - shape: (batchsize x 2) - Samples from a distribution q.
+    :param p: (FloatTensor) - shape: (batchsize x featuresize) - Samples from a distribution p.
+    :param q: (FloatTensor) - shape: (batchsize x featuresize) - Samples from a distribution q.
     :param critic: (Module) - torch module used to compute the Squared Hellinger.
     :return: (FloatTensor) - shape: (1,) - Estimate of the Squared Hellinger
     """
